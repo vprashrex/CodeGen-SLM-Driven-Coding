90b78ea2124a31b9ba79dacc1b31472d57d008e7
Prashant Vasudevan <vprashant5050@gmail.com>
2023-12-26 21:27:52 +0530

    dashboard-changes

--- dashboard/src/components/Banner.js
+++ dashboard/src/components/Banner.js
@@ -1,53 +1,11 @@
-import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import { ArrowRightCircle } from 'react-bootstrap-icons';
-
+import Typewriter from "typewriter-effect";
 
 export const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
-  const toRotate = [ "CodeGen ...", "Code AutoComplete ..." ];
-  const period = 1000;
-
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker) };
-  }, [text])
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === '') {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex(prevIndex => prevIndex + 1);
-    }
-  }
-
   return (
     <section className="banner" id="home">
       <Container>
@@ -57,11 +15,26 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline" style={{marginLeft:'20px'}}>SLM Driven Coding</span>
-                <h1><span style={{marginLeft:'20px'}} className="txt-rotate" dataPeriod="1000" data-rotate='[ "CodeGen", "Code AutoComplete"]'><span className="wrap">{text}</span></span></h1>
+                <h1>
+                  <span>
+                    <span className="wrap">
+                      <Typewriter
+                        options={{
+                          strings: [
+                          "CodeGen",
+                          "Code AutoComplete"
+                          ],
+                          autoStart: true,
+                          loop: true,
+                          deleteSpeed: 50,
+                        }}/>
+                      </span>
+                  </span>
+                </h1>
                 
                 <span className="code-gen">
                   <button onClick={() => window.location.href="http://127.0.0.1:8000/codegen"} className="vvd"><span>CodeGen <ArrowRightCircle size={25} /></span></button>
-                  <button style={{marginLeft:'80px'}} onClick={() => console.log('connect')} className="vvd"><span>Code AutoComplete <ArrowRightCircle size={25} /></span></button>
+                  <button style={{marginLeft:'80px'}} onClick={() => window.location.href="http://127.0.0.1:8000/autocomplete"} className="vvd"><span>Code AutoComplete <ArrowRightCircle size={25} /></span></button>
 
                 </span>
               </div>}
--- dashboard/src/components/Newsletter.js
+++ dashboard/src/components/Newsletter.js
@@ -1,60 +1,27 @@
-import { useState, useEffect } from "react";
 import { Col } from "react-bootstrap";
+import Typewriter from "typewriter-effect";
 
 export const Newsletter = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
-  const [opacity, setOpacity] = useState(1);  // Define opacity state
-  const [delta, setDelta] = useState(150);
-  const [index, setIndex] = useState(1);
-  const toRotate = ["Print the Fibonacci series using the recursive method", "Convert Celsius to Fahrenheit."];
-  const period = 2000;
-
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick();
-    }, delta);
-
-    return () => { clearInterval(ticker) };
-  }, [text, opacity]);
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta((prevDelta) => prevDelta / 2);
-    }
-
-    if ((!isDeleting && updatedText === fullText) || (isDeleting && updatedText === '')) {
-      setIsDeleting(!isDeleting);
-      setLoopNum(isDeleting ? loopNum + 1 : loopNum);
-      setIndex(isDeleting ? 1 : index - 1);
-      setDelta(isDeleting ? 500 : period);
-
-      // If typing is completed, start fading out the text
-      if (!isDeleting) {
-        setTimeout(() => {
-          setOpacity(0);
-        }, 500); // Adjust the duration as needed
-      } else {
-        setOpacity(1); // Reset opacity when starting a new text
-      }
-    } else {
-      setIndex((prevIndex) => prevIndex + (isDeleting ? 0 : 1));
-    }
-  }
+  
   
   return (
       <Col lg={12}>
         <div className="newsletter-bx wow slideInUp">
               <h3>Enter Prompt<br></br></h3>
-              <h1 className="typing-text"><span className="txt-rotate" dataPeriod="2000" data-rotate='[ "Print the Fibonacci series using the recursive method", "Convert Celsius to Fahrenheit."]'><span className="wrap">{text}</span></span></h1>
 
+              <h1 className="typing-text">
+                <Typewriter
+                  options={{
+                    strings: [
+                      "Print the Fibonacci series using the recursive method",
+                      "Convert Celsius to Fahrenheit."
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    deleteSpeed: 50,
+                  }}
+                />
+              </h1>
               
         </div>
       </Col>
